dd5322803e3cc0e6b1d18fec2ce0000ea88650f8
user <theslavicbear@gmail.com>
2019-03-10 19:32:30 -0400

    Added --sum option

--- Randomizer.py
+++ Randomizer.py
@@ -11,6 +11,9 @@ def stats(output):
         print('Mode: '+str(statistics.mode(output)))
     except:
         pass
+    osum(output)
+    
+def osum(output):
     print('Sum: '+str(sum(output)))
 
 parser=argparse.ArgumentParser()
@@ -18,7 +21,8 @@ parser.add_argument('-a', help='Sets lower limit, default 1 or 0 when not specif
 parser.add_argument('numDigits', help='Number of random values to get', type=int, nargs='?', default=1)
 parser.add_argument('-b', help='The upper limit for the randomizer. Default 6',type=int, default=6)
 parser.add_argument('-z', help='Sets to a 0-9 spread rather than default 1-6. overrides -a,-b', action='store_true')
-parser.add_argument('-s', '--stats', help='Prints sum of random numbers', action='store_true')
+parser.add_argument('-s', '--stats', help='Prints statistics of random numbers', action='store_true')
+parser.add_argument('--sum', help='Prints sum of generated values', action='store_true')
 parser.add_argument('--sort', help='Sorts values lowest to highest', action='store_true')
 parser.add_argument('--rsort', help='Sorts values highest to lowest', action='store_true')
 args=parser.parse_args()
@@ -35,3 +39,5 @@ elif args.rsort:
 print(output)
 if args.stats:
     stats(output)
+elif args.sum:
+    osum(output)
